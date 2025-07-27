b210db758381f077e6a1ae92228726ef503d1cdc
Miguel Andres MF <miguelandresmarinf@gmail.com>
2025-07-26 19:16:35 -0500

    Commit inicial

---
+++ src/test/java/com/alura/bookverse/BookVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.bookverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
